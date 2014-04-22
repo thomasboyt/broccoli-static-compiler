4821660c9c6408b822192f5c3b3d17d15124c6d5
Thomas Boyt <thomas.boyt@venmo.com>
2014-04-22 15:40:26 -0400

    Support exclusion patterns in multiglob

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+/* jshint asi: true */
+
 var path = require('path')
 var mkdirp = require('mkdirp')
 var helpers = require('broccoli-kitchen-sink-helpers')
 var Writer = require('broccoli-writer')
+var globSync = require('glob').sync
 
 module.exports = StaticCompiler
 StaticCompiler.prototype = Object.create(Writer.prototype)
@@ -22,7 +25,7 @@ StaticCompiler.prototype.write = function (readTree, destDir) {
         path.join(destDir, self.options.destDir))
     } else {
       var baseDir = path.join(srcDir, self.options.srcDir)
-      var files = helpers.multiGlob(self.options.files, {
+      var files = multiGlob(self.options.files, {
         cwd: baseDir,
         root: baseDir,
         nomount: false
@@ -35,4 +38,59 @@ StaticCompiler.prototype.write = function (readTree, destDir) {
       }
     }
   })
+}
+
+function multiGlob(globs, globOptions) {
+  var options = {
+    nomount: true,
+    strict: true
+  }
+  for (var key in globOptions) {
+    if (globOptions.hasOwnProperty(key)) {
+      options[key] = globOptions[key]
+    }
+  }
+
+  var pathSet = {}
+  var paths = []
+
+  for (var i = 0; i < globs.length; i++) {
+    var glob = globs[i];
+
+    // handle exclusion patterns ('!foo.js')
+    var exclusion = glob.indexOf('!') === 0
+    if (exclusion) {
+      glob = glob.slice(1);
+    }
+
+    if (options.nomount && glob[0] === '/') {
+      throw new Error('Absolute paths not allowed (`nomount` is enabled): ' + globs[i])
+    }
+    var matches = globSync(glob, options)
+    if (matches.length === 0) {
+      throw new Error('Path or pattern "' + glob + '" did not match any files')
+    }
+
+    var j
+    if (exclusion) {
+      for (j = 0; j < matches.length; j++) {
+        if (pathSet[matches[j]]) {
+          // remove excluded glob from paths
+          paths.push(matches[j])
+          paths.splice(paths.indexOf(matches[j]), 1);
+        } else {
+          // mark as included so no further glob will try to append it
+          pathSet[matches[j]] = true
+        }
+      }
+    } else {
+      for (j = 0; j < matches.length; j++) {
+        if (!pathSet[matches[j]]) {
+          pathSet[matches[j]] = true
+          paths.push(matches[j])
+        }
+      }
+    }
+  }
+  return paths
 }
